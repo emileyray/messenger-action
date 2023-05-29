ccc6d990fd42bf8027d909efe75c1d752e8f0c93
Глеб Осотов <g.osotov@along.pw>
2023-05-29 21:21:43 +0300

    ci-test (#2)
    Co-authored-by: glebosotov <gleb.osotov@gmail.com>
    Reviewed-on: https://git.lipsar.studio/AL-Mobile-Base/messenger-action/pulls/2

--- message.py
+++ message.py
@@ -6,7 +6,7 @@ import httpx
 
 
 class Config:
-    def __init__(self, telegram_token, telegram_chat_id, rocket_chat_url, rocket_chat_token, rocket_chat_user_id, rocket_chat_channel, message_text, enable_telegram = True, enable_rocket_chat = True):
+    def __init__(self, telegram_token, telegram_chat_id, rocket_chat_url, rocket_chat_token, rocket_chat_user_id, rocket_chat_channel, message_text, enable_telegram = 'false', enable_rocket_chat = 'false'):
         self.telegram_token = telegram_token
         self.telegram_chat_id = telegram_chat_id
         self.rocket_chat_url = rocket_chat_url
@@ -25,14 +25,14 @@ def get_vars() -> Config:
     # dotenv.load_dotenv()
     # print(os.environ)
     return Config(
-        telegram_token = os.environ['INPUT_TELEGRAM-TOKEN'],
-        telegram_chat_id = os.environ['INPUT_TELEGRAM-CHAT-ID'],
-        rocket_chat_url = os.environ['INPUT_ROCKET-CHAT-URL'],
-        rocket_chat_token = os.environ['INPUT_ROCKET-CHAT-TOKEN'],
-        rocket_chat_user_id = os.environ['INPUT_ROCKET-CHAT-USERID'],
-        rocket_chat_channel = os.environ['INPUT_ROCKET-CHAT-TARGET-CHAT'],
-        message_text = os.environ['INPUT_MESSAGE-TEXT'],
-        enable_rocket_chat = os.environ['INPUT_ROCKET-CHAT'],
+        telegram_token = os.environ['INPUT_TELEGRAM_TOKEN'],
+        telegram_chat_id = os.environ['INPUT_TELEGRAM_TO'],
+        rocket_chat_url = os.environ['INPUT_ROCKET_CHAT_URL'],
+        rocket_chat_token = os.environ['INPUT_ROCKET_CHAT_TOKEN'],
+        rocket_chat_user_id = os.environ['INPUT_ROCKET_CHAT_USERID'],
+        rocket_chat_channel = os.environ['INPUT_ROCKET_CHAT_TARGET_CHAT'],
+        message_text = os.environ['INPUT_MESSAGE_TEXT'],
+        enable_rocket_chat = os.environ['INPUT_ROCKET_CHAT'],
         enable_telegram = os.environ['INPUT_TELEGRAM'],
     )
 
@@ -40,10 +40,11 @@ def send_telegram_message(config: Config):
     print(config)
     assert config.enable_telegram == 'true' and config.telegram_token and config.telegram_chat_id and config.message_text
     url = f'https://api.telegram.org/bot{config.telegram_token}/sendMessage'
+    text = config.message_text.replace("\\n", "\n")
     data = {
         'chat_id': config.telegram_chat_id,
-        'text': config.message_text,
-        'parse_mode': 'HTML'
+        'text': text,
+        'parse_mode': 'markdown'
     }
     response = httpx.post(url, json=data)
     if response.status_code != 200:
@@ -52,7 +53,7 @@ def send_telegram_message(config: Config):
         sys.exit(1)
 
 def send_rocket_chat_message(config: Config):
-    assert config.enable_rocket_chat == 'true' and config.rocket_chat_url and config.rocket_chat_token and config.rocket_chat_user_id and config.rocket_chat_channel and config.message_text != None
+    assert config.enable_rocket_chat == 'true' and config.rocket_chat_url and config.rocket_chat_token and config.rocket_chat_user_id and config.rocket_chat_channel and config.message_text
     url = f'{config.rocket_chat_url}/api/v1/chat.postMessage'
     headers = {
         'X-Auth-Token': config.rocket_chat_token,
@@ -74,9 +75,9 @@ def send_rocket_chat_message(config: Config):
 
 def main():
     config = get_vars()
-    if config.enable_telegram:
+    if config.enable_telegram == 'true':
         send_telegram_message(config)
-    if config.enable_rocket_chat:
+    if config.enable_rocket_chat == 'true':
         send_rocket_chat_message(config)
 
 if __name__ == '__main__':
